4dfc2b05d2e53933eaad304c1ebc029ce2598b44
Jason Forest <jason.forest@glyphe.org>
2019-10-14 14:49:03 +0100

    Current config with i3, polybar and termite.

---
+++ bin/launch_polybar.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar example &
+done
